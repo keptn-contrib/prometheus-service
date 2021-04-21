264fc7fa81bb84910a4e4a8c13b1e900706b8fba
rajdas98 <mail.rajdas@gmail.com>
2021-04-22 00:31:01 +0530

    Adding Prometheus SLI functionalities to promtheus-services
    Signed-off-by: rajdas98 <mail.rajdas@gmail.com>

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"io/ioutil"
+
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/cloudevents/sdk-go/v2/types"
 	"github.com/google/uuid"
 	"github.com/keptn-contrib/prometheus-service/eventhandling"
 	"github.com/keptn-contrib/prometheus-service/utils"
-	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v2"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 	keptncommon "github.com/keptn/go-utils/pkg/lib/keptn"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
+
+const eventbroker = "EVENTBROKER"
+const sliResourceURI = "prometheus/sli.yaml"
+const serviceName = "prometheus-sli-service"
 
 type envConfig struct {
 	// Port on which to listen for cloudevents
@@ -25,9 +40,17 @@ type envConfig struct {
 	Path string `envconfig:"RCV_PATH" default:"/"`
 }
 
+type prometheusCredentials struct {
+	URL      string `json:"url" yaml:"url"`
+	User     string `json:"user" yaml:"user"`
+	Password string `json:"password" yaml:"password"`
+}
+
 type ceTest struct {
 	Specversion string `json:"specversion" yaml:"specversion"`
 }
+
+var namespace = os.Getenv("POD_NAMESPACE")
 
 func main() {
 	// listen on port 8080 for any event
@@ -64,6 +87,10 @@ func _main(env envConfig) int {
 func gotEvent(event cloudevents.Event) error {
 	var shkeptncontext string
 	_ = event.Context.ExtensionAs("shkeptncontext", &shkeptncontext)
+
+	if event.Type() == keptnv2.GetTriggeredEventType(keptnv2.GetSLITaskName) {
+		return processEvent(event)
+	}
 
 	logger := keptncommon.NewLogger(shkeptncontext, event.Context.GetID(), "prometheus-service")
 
@@ -122,4 +149,243 @@ func Handler(rw http.ResponseWriter, req *http.Request) {
 			rw.WriteHeader(201)
 		}
 	}
+}
+
+func processEvent(event cloudevents.Event) error {
+
+	eventData := &keptnv2.GetSLITriggeredEventData{}
+	err := event.DataAs(eventData)
+	if err != nil {
+		return err
+	}
+
+	keptnCtx, err := types.ToString(event.Context.GetExtensions()["shkeptncontext"])
+	if err != nil {
+		return fmt.Errorf("could not determine keptnContext of input event: %s", err.Error())
+	}
+
+	log := keptncommon.NewLogger(keptnCtx, event.Context.GetID(), serviceName)
+
+	// don't continue if SLIProvider is not prometheus
+	if eventData.GetSLI.SLIProvider != "prometheus" {
+		return nil
+	}
+
+	// 1: send .started event
+	var sliResults = []*keptnv2.SLIResult{}
+	if err = sendGetSLIStartedEvent(event, eventData, keptnCtx); err != nil {
+		return sendGetSLIFinishedEvent(event, eventData, sliResults, err, keptnCtx)
+	}
+
+	// 2: try to fetch metrics
+	if sliResults, err = retrieveMetrics(event, eventData, log); err != nil {
+		return sendGetSLIFinishedEvent(event, eventData, sliResults, err, keptnCtx)
+	}
+
+	// 3: send .finished event
+	return sendGetSLIFinishedEvent(event, eventData, sliResults, nil, keptnCtx)
+}
+
+func retrieveMetrics(event cloudevents.Event, eventData *keptnv2.GetSLITriggeredEventData, log keptncommon.LoggerInterface) ([]*keptnv2.SLIResult, error) {
+	log.Info("Retrieving Prometheus metrics")
+
+	clusterConfig, err := rest.InClusterConfig()
+	if err != nil {
+		log.Error("could not create Kubernetes cluster config")
+		return nil, errors.New("could not create Kubernetes client")
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(clusterConfig)
+	if err != nil {
+		log.Error("could not create Kubernetes client")
+		return nil, errors.New("could not create Kubernetes client")
+	}
+
+	prometheusApiURL, err := getPrometheusApiURL(eventData.Project, kubeClient.CoreV1(), log)
+	if err != nil {
+		return nil, err
+	}
+
+	eventBrokerURL := os.Getenv(eventbroker)
+	if eventBrokerURL == "" {
+		eventBrokerURL = "http://event-broker/keptn"
+	}
+
+	keptnHandler, err := keptnv2.NewKeptn(&event, keptncommon.KeptnOpts{EventBrokerURL: eventBrokerURL})
+	if err != nil {
+		return nil, err
+	}
+
+	prometheusHandler := utils.NewPrometheusHandler(prometheusApiURL, eventData.Project, eventData.Stage, eventData.Service, eventData.GetSLI.CustomFilters)
+
+	projectCustomQueries, err := getCustomQueries(keptnHandler, eventData.Project, eventData.Stage, eventData.Service, log)
+	if err != nil {
+		log.Error("Failed to get custom queries for project " + eventData.Project)
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	if projectCustomQueries != nil {
+		prometheusHandler.CustomQueries = projectCustomQueries
+	}
+
+	var sliResults []*keptnv2.SLIResult
+
+	for _, indicator := range eventData.GetSLI.Indicators {
+		log.Info("Fetching indicator: " + indicator)
+		sliValue, err := prometheusHandler.GetSLIValue(indicator, eventData.GetSLI.Start, eventData.GetSLI.End, log)
+		if err != nil {
+			sliResults = append(sliResults, &keptnv2.SLIResult{
+				Metric:  indicator,
+				Value:   0,
+				Success: false,
+				Message: err.Error(),
+			})
+		} else if math.IsNaN(sliValue) {
+			sliResults = append(sliResults, &keptnv2.SLIResult{
+				Metric:  indicator,
+				Value:   0,
+				Success: false,
+				Message: "SLI value is NaN",
+			})
+		} else {
+			sliResults = append(sliResults, &keptnv2.SLIResult{
+				Metric:  indicator,
+				Value:   sliValue,
+				Success: true,
+			})
+		}
+	}
+	return sliResults, nil
+}
+
+func getCustomQueries(keptnHandler *keptnv2.Keptn, project string, stage string, service string, logger keptncommon.LoggerInterface) (map[string]string, error) {
+	logger.Info("Checking for custom SLI queries")
+
+	customQueries, err := keptnHandler.GetSLIConfiguration(project, stage, service, sliResourceURI)
+	if err != nil {
+		return nil, err
+	}
+
+	return customQueries, nil
+}
+
+func getPrometheusApiURL(project string, kubeClient v1.CoreV1Interface, logger keptncommon.LoggerInterface) (string, error) {
+	logger.Info("Checking if external prometheus instance has been defined for project " + project)
+	secret, err := kubeClient.Secrets(namespace).Get("prometheus-credentials-"+project, metav1.GetOptions{})
+
+	// return cluster-internal prometheus URL if no secret has been found
+	if err != nil {
+		logger.Info("could not retrieve or read secret: " + err.Error())
+		logger.Info("No external prometheus instance defined for project " + project + ". Using default: http://prometheus-service.monitoring.svc.cluster.local:8080")
+		return "http://prometheus-service.monitoring.svc.cluster.local:8080", nil
+	}
+
+	pc := &prometheusCredentials{}
+	err = yaml.Unmarshal(secret.Data["prometheus-credentials"], pc)
+
+	if err != nil {
+		logger.Error("Could not parse credentials for external prometheus instance: " + err.Error())
+		return "", errors.New("invalid credentials format found in secret 'prometheus-credentials-" + project)
+	}
+	logger.Info("Using external prometheus instance for project " + project + ": " + pc.URL)
+	prometheusURL := generatePrometheusURL(pc)
+
+	return prometheusURL, nil
+}
+
+func generatePrometheusURL(pc *prometheusCredentials) string {
+	prometheusURL := pc.URL
+
+	credentialsString := ""
+
+	if pc.User != "" && pc.Password != "" {
+		credentialsString = url.QueryEscape(pc.User) + ":" + url.QueryEscape(pc.Password) + "@"
+	}
+	if strings.HasPrefix(prometheusURL, "https://") {
+		prometheusURL = strings.TrimPrefix(prometheusURL, "https://")
+		prometheusURL = "https://" + credentialsString + prometheusURL
+	} else if strings.HasPrefix(prometheusURL, "http://") {
+		prometheusURL = strings.TrimPrefix(prometheusURL, "http://")
+		prometheusURL = "http://" + credentialsString + prometheusURL
+	} else {
+		// assume https transport
+		prometheusURL = "https://" + credentialsString + prometheusURL
+	}
+	return strings.Replace(prometheusURL, " ", "", -1)
+}
+
+func sendGetSLIStartedEvent(inputEvent cloudevents.Event, eventData *keptnv2.GetSLITriggeredEventData, keptnContext interface{}) error {
+
+	source, _ := url.Parse(serviceName)
+
+	getSLIStartedEvent := keptnv2.GetSLIStartedEventData{
+		EventData: keptnv2.EventData{
+			Project: eventData.Project,
+			Stage:   eventData.Stage,
+			Service: eventData.Service,
+			Labels:  eventData.Labels,
+			Status:  keptnv2.StatusSucceeded,
+			Result:  keptnv2.ResultPass,
+		},
+	}
+
+	event := cloudevents.NewEvent()
+	event.SetType(keptnv2.GetStartedEventType(keptnv2.GetSLITaskName))
+	event.SetSource(source.String())
+	event.SetDataContentType(cloudevents.ApplicationJSON)
+	event.SetExtension("shkeptncontext", keptnContext)
+	event.SetExtension("triggeredid", inputEvent.ID())
+	event.SetData(cloudevents.ApplicationJSON, getSLIStartedEvent)
+
+	return sendEvent(event)
+}
+
+func sendGetSLIFinishedEvent(inputEvent cloudevents.Event, eventData *keptnv2.GetSLITriggeredEventData, indicatorValues []*keptnv2.SLIResult, err error, keptnContext interface{}) error {
+	source, _ := url.Parse(serviceName)
+	var status = keptnv2.StatusSucceeded
+	var result = keptnv2.ResultPass
+	var message = ""
+
+	if err != nil {
+		status = keptnv2.StatusErrored
+		result = keptnv2.ResultFailed
+		message = err.Error()
+	}
+
+	getSLIEvent := keptnv2.GetSLIFinishedEventData{
+		EventData: keptnv2.EventData{
+			Project: eventData.Project,
+			Stage:   eventData.Stage,
+			Service: eventData.Service,
+			Labels:  eventData.Labels,
+			Status:  status,
+			Result:  result,
+			Message: message,
+		},
+		GetSLI: keptnv2.GetSLIFinished{
+			IndicatorValues: indicatorValues,
+			Start:           eventData.GetSLI.Start,
+			End:             eventData.GetSLI.End,
+		},
+	}
+
+	event := cloudevents.NewEvent()
+	event.SetType(keptnv2.GetFinishedEventType(keptnv2.GetSLITaskName))
+	event.SetSource(source.String())
+	event.SetDataContentType(cloudevents.ApplicationJSON)
+	event.SetExtension("shkeptncontext", keptnContext)
+	event.SetExtension("triggeredid", inputEvent.ID())
+	event.SetData(cloudevents.ApplicationJSON, getSLIEvent)
+
+	return sendEvent(event)
+}
+
+func sendEvent(event cloudevents.Event) error {
+	keptnHandler, err := keptnv2.NewKeptn(&event, keptncommon.KeptnOpts{})
+	if err != nil {
+		return err
+	}
+
+	return keptnHandler.SendCloudEvent(event)
 }
--- utils/prometheus.go
+++ utils/prometheus.go
@@ -1,15 +1,59 @@
 package utils
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"errors"
 	"fmt"
+
+	keptncommon "github.com/keptn/go-utils/pkg/lib/keptn"
 	alertConfig "github.com/prometheus/alertmanager/config"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"math"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
+	"net/http"
 )
+
+const Throughput = "throughput"
+const ErrorRate = "error_rate"
+const RequestLatencyP50 = "response_time_p50"
+const RequestLatencyP90 = "response_time_p90"
+const RequestLatencyP95 = "response_time_p95"
+
+type prometheusResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric struct {
+			} `json:"metric"`
+			Value []interface{} `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
+// Handler interacts with a prometheus API endpoint
+type Handler struct {
+	ApiURL        string
+	Username      string
+	Password      string
+	Project       string
+	Stage         string
+	Service       string
+	HTTPClient    *http.Client
+	CustomFilters []*keptnv2.SLIFilter
+	CustomQueries map[string]string
+}
 
 const alertManagerYml = `global:
 templates:
@@ -312,4 +356,312 @@ func (p *PrometheusHelper) UpdateAMConfigMap(name string, filename string, names
 	getCM.Data[filename] = fmt.Sprint(config)
 
 	return p.UpdateConfigMap(getCM, namespace)
+}
+
+// NewPrometheusHandler returns a new prometheus handler that interacts with the Prometheus REST API
+func NewPrometheusHandler(apiURL string, project string, stage string, service string, customFilters []*keptnv2.SLIFilter) *Handler {
+	ph := &Handler{
+		ApiURL:        apiURL,
+		Project:       project,
+		Stage:         stage,
+		Service:       service,
+		HTTPClient:    &http.Client{},
+		CustomFilters: customFilters,
+	}
+
+	return ph
+}
+
+// GetSLIValue retrieves the specified value via the Prometheus API
+func (ph *Handler) GetSLIValue(metric string, start string, end string, logger keptncommon.LoggerInterface) (float64, error) {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	startUnix, err := parseUnixTimestamp(start)
+	if err != nil {
+		return 0, err
+	}
+	endUnix, _ := parseUnixTimestamp(end)
+	if err != nil {
+		return 0, err
+	}
+	query, err := ph.getMetricQuery(metric, startUnix, endUnix)
+	if err != nil {
+		return 0, err
+	}
+	queryString := ph.ApiURL + "/api/v1/query?query=" + url.QueryEscape(query) + "&time=" + strconv.FormatInt(endUnix.Unix(), 10)
+	logger.Info("Generated query: /api/v1/query?query=" + query + "&time=" + strconv.FormatInt(endUnix.Unix(), 10))
+
+	req, err := http.NewRequest("GET", queryString, nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := ph.HTTPClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		return 0, errors.New("metric could not be received")
+	}
+
+	prometheusResult := &prometheusResponse{}
+
+	err = json.Unmarshal(body, prometheusResult)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(prometheusResult.Data.Result) == 0 || len(prometheusResult.Data.Result[0].Value) == 0 {
+		logger.Info("Prometheus Result is 0, returning value 0")
+		// for the error rate query, the result is received with no value if the error rate is 0, so we have to assume that's OK at this point
+		return 0, nil
+	}
+
+	parsedValue := fmt.Sprintf("%v", prometheusResult.Data.Result[0].Value[1])
+	floatValue, err := strconv.ParseFloat(parsedValue, 64)
+	logger.Info(fmt.Sprintf("Prometheus Result is %v", floatValue))
+	if err != nil {
+		return 0, nil
+	}
+	return floatValue, nil
+}
+
+func (ph *Handler) getMetricQuery(metric string, start time.Time, end time.Time) (string, error) {
+	query := ph.CustomQueries[metric]
+	if query != "" {
+		query = ph.replaceQueryParameters(query, start, end)
+
+		return query, nil
+	}
+
+	switch metric {
+	case Throughput:
+		return ph.getThroughputQuery(start, end), nil
+	case ErrorRate:
+		return ph.getErrorRateQuery(start, end), nil
+	case RequestLatencyP50:
+		return ph.getRequestLatencyQuery("50", start, end), nil
+	case RequestLatencyP90:
+		return ph.getRequestLatencyQuery("90", start, end), nil
+	case RequestLatencyP95:
+		return ph.getRequestLatencyQuery("95", start, end), nil
+	default:
+		return "", errors.New("unsupported SLI")
+	}
+}
+
+func (ph *Handler) replaceQueryParameters(query string, start time.Time, end time.Time) string {
+	for _, filter := range ph.CustomFilters {
+		filter.Value = strings.Replace(filter.Value, "'", "", -1)
+		filter.Value = strings.Replace(filter.Value, "\"", "", -1)
+		query = strings.Replace(query, "$"+filter.Key, filter.Value, -1)
+		query = strings.Replace(query, "$"+strings.ToUpper(filter.Key), filter.Value, -1)
+	}
+	query = strings.Replace(query, "$PROJECT", ph.Project, -1)
+	query = strings.Replace(query, "$STAGE", ph.Stage, -1)
+	query = strings.Replace(query, "$SERVICE", ph.Service, -1)
+	query = strings.Replace(query, "$project", ph.Project, -1)
+	query = strings.Replace(query, "$stage", ph.Stage, -1)
+	query = strings.Replace(query, "$service", ph.Service, -1)
+	durationString := strconv.FormatInt(getDurationInSeconds(start, end), 10) + "s"
+
+	query = strings.Replace(query, "$DURATION_SECONDS", durationString, -1)
+	return query
+}
+
+func (ph *Handler) getThroughputQuery(start time.Time, end time.Time) string {
+	if ph.CustomQueries != nil && ph.CustomQueries["throughput"] != "" {
+		query := ph.CustomQueries["throughput"]
+		query = ph.replaceQueryParameters(query, start, end)
+		return query
+	}
+	return ph.getDefaultThroughputQuery(start, end)
+}
+
+func (ph *Handler) getDefaultThroughputQuery(start time.Time, end time.Time) string {
+	filterExpr := ph.getDefaultFilterExpression()
+	durationString := strconv.FormatInt(getDurationInSeconds(start, end), 10) + "s"
+	// e.g. sum(rate(http_requests_total{job="carts-sockshop-dev"}[30m]))&time=1571649085
+	/*
+		{
+		    "status": "success",
+		    "data": {
+		        "resultType": "vector",
+		        "result": [
+		            {
+		                "metric": {},
+		                "value": [
+		                    1571649085,
+		                    "0.20111420612813372"
+		                ]
+		            }
+		        ]
+		    }
+		}
+	*/
+	return "sum(rate(http_requests_total{" + filterExpr + "}[" + durationString + "]))"
+}
+
+func (ph *Handler) getErrorRateQuery(start time.Time, end time.Time) string {
+	if ph.CustomQueries != nil && ph.CustomQueries["error_rate"] != "" {
+		query := ph.CustomQueries["error_rate"]
+		query = ph.replaceQueryParameters(query, start, end)
+		return query
+	}
+	return ph.getDefaultErrorRateQuery(start, end)
+}
+
+func (ph *Handler) getDefaultErrorRateQuery(start time.Time, end time.Time) string {
+	filterExpr := ph.getDefaultFilterExpression()
+	durationString := strconv.FormatInt(getDurationInSeconds(start, end), 10) + "s"
+	// e.g. sum(rate(http_requests_total{job="carts-sockshop-dev",status!~'2..'}[30m]))/sum(rate(http_requests_total{job="carts-sockshop-dev"}[30m]))&time=1571649085
+	/*
+		with value:
+		{
+		    "status": "success",
+		    "data": {
+		        "resultType": "vector",
+		        "result": [
+		            {
+		                "metric": {},
+		                "value": [
+		                    1571649085,
+		                    "1.00505917125441"
+		                ]
+		            }
+		        ]
+		    }
+		}
+
+		no value (error rate 0):
+		{
+		    "status": "success",
+		    "data": {
+		        "resultType": "vector",
+		        "result": []
+		    }
+		}
+	*/
+	return "sum(rate(http_requests_total{" + filterExpr + ",status!~'2..'}[" + durationString + "]))/sum(rate(http_requests_total{" + filterExpr + "}[" + durationString + "]))"
+}
+
+func (ph *Handler) getRequestLatencyQuery(percentile string, start time.Time, end time.Time) string {
+	if ph.CustomQueries != nil {
+		query := ""
+		switch percentile {
+		case "50":
+			query = ph.CustomQueries["response_time_p50"]
+			break
+		case "90":
+			query = ph.CustomQueries["response_time_p90"]
+			break
+		case "95":
+			query = ph.CustomQueries["response_time_p95"]
+			break
+		default:
+			query = ""
+		}
+		if query != "" {
+			query = ph.replaceQueryParameters(query, start, end)
+			return query
+		}
+	}
+	return ph.getDefaultRequestLatencyQuery(start, end, percentile)
+}
+
+func (ph *Handler) getDefaultRequestLatencyQuery(start time.Time, end time.Time, percentile string) string {
+	filterExpr := ph.getDefaultFilterExpression()
+	durationString := strconv.FormatInt(getDurationInSeconds(start, end), 10) + "s"
+	// e.g. histogram_quantile(0.95, sum(rate(http_response_time_milliseconds_bucket{job='carts-sockshop-dev'}[30m])) by (le))&time=1571649085
+	/*
+		{
+		    "status": "success",
+		    "data": {
+		        "resultType": "vector",
+		        "result": [
+		            {
+		                "metric": {},
+		                "value": [
+		                    1571649085,
+		                    "4.607481671642585"
+		                ]
+		            }
+		        ]
+		    }
+		}
+	*/
+	return "histogram_quantile(0." + percentile + ",sum(rate(http_response_time_milliseconds_bucket{" + filterExpr + "}[" + durationString + "]))by(le))"
+}
+
+func (ph *Handler) getDefaultFilterExpression() string {
+	filterExpression := ""
+	jobFilterFound := false
+	if ph.CustomFilters != nil && len(ph.CustomFilters) > 0 {
+		for _, filter := range ph.CustomFilters {
+			if filter.Key == "job" {
+				jobFilterFound = true
+			}
+			/* if no operator has been included in the label filter, use exact matching (=), e.g.
+			e.g.:
+			key: handler
+			value: ItemsController
+			*/
+			if !strings.HasPrefix(filter.Value, "=") && !strings.HasPrefix(filter.Value, "!=") && !strings.HasPrefix(filter.Value, "=~") && !strings.HasPrefix(filter.Value, "!~") {
+				filter.Value = strings.Replace(filter.Value, "'", "", -1)
+				filter.Value = strings.Replace(filter.Value, "\"", "", -1)
+				if filterExpression != "" {
+					filterExpression = filterExpression + "," + filter.Key + "='" + filter.Value + "'"
+				} else {
+					filterExpression = filter.Key + "='" + filter.Value + "'"
+				}
+
+			} else {
+				/* if a valid operator (=, !=, =~, !~) is prepended to the value, use that one
+				e.g.:
+				key: handler
+				value: !=HealthCheckController
+
+				OR
+
+				key: handler
+				value: =~.+ItemsController|.+VersionController
+				*/
+				filter.Value = strings.Replace(filter.Value, "\"", "'", -1)
+				if filterExpression != "" {
+					filterExpression = filterExpression + "," + filter.Key + filter.Value
+				} else {
+					filterExpression = filter.Key + filter.Value
+				}
+			}
+		}
+	}
+	if !jobFilterFound {
+		if filterExpression != "" {
+			filterExpression = "job='" + ph.Service + "-" + ph.Project + "-" + ph.Stage + "-canary'" + "," + filterExpression
+		} else {
+			filterExpression = "job='" + ph.Service + "-" + ph.Project + "-" + ph.Stage + "-canary'"
+		}
+
+	}
+	return filterExpression
+}
+
+func parseUnixTimestamp(timestamp string) (time.Time, error) {
+	parsedTime, err := time.Parse(time.RFC3339, timestamp)
+	if err == nil {
+		return parsedTime, nil
+	}
+
+	timestampInt, err := strconv.ParseInt(timestamp, 10, 64)
+	if err != nil {
+		return time.Now(), err
+	}
+	unix := time.Unix(timestampInt, 0)
+	return unix, nil
+}
+
+func getDurationInSeconds(start, end time.Time) int64 {
+	seconds := end.Sub(start).Seconds()
+	return int64(math.Ceil(seconds))
 }
